4a9ddb2325dc7c27b8a1c945fdae72244358db1d
ciberseguranca <nunocarreirobento@enta.pt>
2024-06-19 17:20:06 +0000

    Update

---
+++ cert.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source public-ip-addresses
+
+kubectl create secret generic nuno.myftp.biz \
+  --from-file=fullchain.pem=../nuno.myftp.biz/fullchain.pem \
+  --from-file=privkey.pem=../nuno.myftp.biz/privkey.pem \
+  -n default
+# Apply the Ingress resource
+#kubectl apply -f ingress-resource.yaml
+
+kubectl apply -f nginx-deployment.yaml
+
+#kubectl get all -n ingress-nginx
+
+echo "Ingress resource creation completed."
